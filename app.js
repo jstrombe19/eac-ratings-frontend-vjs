826e0097b12ba8e13f2e11b66effb26155b77fb4
jstrombe19 <jaredstromberg@gmail.com>
2024-04-05 15:48:24 -0700

    basic setup and templated form complete

---
+++ app.js
@@ -0,0 +1,44 @@
+console.log("Good morning, Vietnam!");
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("%cDOM Content Loaded and Parsed!", 'color: magenta');
+
+    // select forms section for later use
+    const formSection = document.getElementById('forms-section');
+    // declare all form types
+    const newUserForm = document.createElement('form');
+    const newRatingForm = document.createElement('form');
+    const loginForm = document.createElement('form');
+    
+    // append all form type sto forms section
+    formSection.appendChild(loginForm);
+    formSection.appendChild(newUserForm);
+    formSection.appendChild(newRatingForm);
+    
+    // create login form
+    let login_username = document.createElement('input');
+    let login_password = document.createElement('input');
+    let login_submit = document.createElement('button');
+    
+    login_username.setAttribute('type', 'text');
+    login_username.setAttribute('class', 'form-input');
+    login_username.setAttribute('placeholder', 'username');
+    login_password.setAttribute('type', 'password');
+    login_password.setAttribute('class', 'form-input');
+    login_password.setAttribute('placeholder', 'password');
+    login_submit.setAttribute('type', 'submit');
+    login_submit.setAttribute('class', 'form-submission');
+    login_submit.innerText = 'login';
+
+    loginForm.appendChild(login_username);
+    loginForm.appendChild(login_password);
+    loginForm.appendChild(login_submit);
+
+    // create new rating form
+    
+    // create new user form
+    
+    // create statistics viewer
+    
+})
